bb9d0b0518fbb01c4daaadea6e0465341af33329
norvikalexyan <norvik.alexyan97@protonmail.com>
2022-03-30 16:51:51 +0400

    examples of displacy

--- tokenization.py
+++ tokenization.py
@@ -1,5 +1,7 @@
 import spacy
 
+from spacy import displacy
+
 nlp = spacy.load('en_core_web_sm')
 
 mystring = '"We\'re moving to L.A.!"'
@@ -28,3 +30,17 @@ doc4 = nlp(u"Autonomous cars shift insurance liability toward manufacturers.")
 
 for chunk in doc4.noun_chunks:
     print(chunk)
+
+doc5 = nlp(u"Apple is going to build a U.K. factory for $6 million.")
+
+displacy.render(doc5, style='dep', jupyter=False, options={'distance': 110}) # it will render and show an image of the tokens
+
+doc6 = nlp(u"Over the last quarter Apple sold nearly 20 thousands ipods for a profit of $6 million.")
+
+displacy.render(doc6, style='ent', jupyter=False, options={'distance': 110}) # it will render and show an image of the tokens
+
+displacy.serve(doc6, style='ent', port=8080)    # create a server to show the image of rendered tokens
+
+doc7 = nlp(u"this is a sentence.")
+
+displacy.serve(doc7, style='dep')
