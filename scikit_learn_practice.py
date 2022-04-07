b657236ab103e267ec56aa32b6d68ee795655b33
norvikalexyan <norvik.alexyan97@protonmail.com>
2022-04-07 17:14:45 +0400

    load a Logistic regression model and train on some dataset, check the amount of unique values of Label column using matplotlib and check the necessary info of dataset, and split the dataset into training set and testing set

---
+++ scikit_learn_practice.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+data_file = pd.read_csv('../UPDATED_NLP_COURSE/TextFiles/smsspamcollection.tsv', sep='\t')
+
+# print(data_file.head())   # show the first 5 rows
+# print(data_file.isnull().sum())   # show empty values in the dataset
+# print(len(data_file))     # length of the dataset
+# print(data_file['label'].unique())    # unique values of label column
+# print(data_file['label'].value_counts())  # amount of unique values in the dataset
+
+plt.xscale('log')
+bins = 1.15 ** (np.arange(0, 50))
+plt.hist(data_file[data_file['label'] == 'ham']['length'], bins=bins, alpha=0.8)
+plt.hist(data_file[data_file['label'] == 'spam']['length'], bins=bins, alpha=0.8)
+plt.legend(('ham', 'spam'))
+plt.show()
+
+# X feature data
+X = data_file[['length', 'punct']]
+
+# y is our label
+y = data_file['label']
+
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# print(x_train.shape)
+# print(x_test.shape)
+
+logistic_regression_model = LogisticRegression(solver='lbfgs')
+
+logistic_regression_model.fit(x_train, y_train)
