faabe35119d2781a9d4becc1105ddb69a03ae9cc
norvikalexyan <norvik.alexyan97@protonmail.com>
2022-04-08 14:26:58 +0400

    train with different models then use pipeline and check the accuracy and predictions

--- text_classification/text_feature_extraction.py
+++ text_classification/text_feature_extraction.py
@@ -1,8 +1,13 @@
 import numpy as np
 import pandas as pd
 
+from sklearn.svm import LinearSVC
+from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
 
 df = pd.read_csv('../UPDATED_NLP_COURSE/TextFiles/smsspamcollection.tsv', sep='\t')
 
@@ -23,4 +28,30 @@ count_vect = CountVectorizer()
 # x_train_count = count_vect.transform(x_train)
 x_train_counts = count_vect.fit_transform(x_train)
 
-# Transform the original text message to vector:
+tfidf_transformer = TfidfTransformer()
+x_train_tfidf = tfidf_transformer.fit_transform(x_train_counts)
+
+# print(x_train_tfidf.shape)
+
+vectorizer = TfidfVectorizer()
+x_train_tfidf = vectorizer.fit_transform(x_train)
+
+clf = LinearSVC()
+clf.fit(x_train_tfidf, y_train)
+
+text_clf = Pipeline([('tfidf', TfidfVectorizer()), ('clf', LinearSVC())])
+
+text_clf.fit(x_train, y_train)
+
+predictins = text_clf.predict(x_test)
+accuracy = confusion_matrix(y_test, predictins)
+
+# print(accuracy)
+
+accuracy = classification_report(y_test, predictins)
+
+# print(accuracy)
+
+accuracy = accuracy_score(y_test, predictins)
+
+print(accuracy)
